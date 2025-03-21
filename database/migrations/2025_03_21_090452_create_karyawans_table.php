cc9b079345900f58ff8a285f6b4d35e4eea2580c
Maryam Fatimah <maryamfatimah4@gmail.com>
2025-03-21 19:26:29 +0700

    revisi tyara

--- database/migrations/2025_03_21_090452_create_karyawans_table.php
+++ database/migrations/2025_03_21_090452_create_karyawans_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('karyawans', function (Blueprint $table) {
-                $table->id('');
+                $table->id();
                 $table->string('id_karyawan');
                 $table->string('kode_karyawan');
                 $table->string('nama');
